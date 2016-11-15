3edb9469ef03af934fa13a56aa7e7964f081c1bf
Andriy Kornienko <kornienko.andriy@gmail.com>
2016-11-15 16:37:19 +0200

    page login and register

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $(".wrap-input input").focusin(function() {
+        $(this).closest('.wrap-input').addClass("focus-input");
+    });
+    $(".wrap-input input").focusout(function() {
+        $(this).closest('.wrap-input').removeClass("focus-input");
+    });
+
+
+});
