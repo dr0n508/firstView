074db619d9dba61b28ee43bd749f299b41296dee
Andriy Kornienko <kornienko.andriy@gmail.com>
2016-11-18 15:57:01 +0200

    add js for slider and map on all page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+//------------focus for input-----//
+
     $(".wrap-input input").focusin(function() {
         $(this).closest('.wrap-input').addClass("focus-input");
     });
@@ -12,10 +14,15 @@ $(document).ready(function () {
         $("#wrapper").toggleClass("toggled");
     });
 
+//------------tooltip on page map-----//
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     });
 
+//-----------------------------------//
+//------------slider walkthrough-----//
+//-----------------------------------//
+    
     $("#owl-demo").owlCarousel({
 
         navigation : true, // Show next and prev buttons
@@ -25,12 +32,38 @@ $(document).ready(function () {
         rewindNav:false,
         navigationText : false
 
-        // "singleItem:true" is a shortcut for:
-        // items : 1,
-        // itemsDesktop : false,
-        // itemsDesktopSmall : false,
-        // itemsTablet: false,
-        // itemsMobile : false
-
     });
+    
+//-----------------------------------//
+//------------map section------------//
+//-----------------------------------//
+
+    if($('#mapid').length) {
+        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw'
+        }).addTo(mymap);
+
+        L.marker([51.5, -0.09]).addTo(mymap);
+
+        L.circle([51.508, -0.11], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 500
+        }).addTo(mymap);
+
+        L.polygon([
+            [51.509, -0.08],
+            [51.503, -0.06],
+            [51.51, -0.047]
+        ]).addTo(mymap);
+    }
+
 });
